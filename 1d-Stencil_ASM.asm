71f7b4f9135936c89afbf3bd7e27079af45c50b9
Stephanie Joy Relles <80933795+its-teph@users.noreply.github.com>
2023-07-17 02:29:01 +0800

    Update 1d-Stencil_ASM.asm

--- 1d-Stencil_ASM.asm
+++ 1d-Stencil_ASM.asm
@@ -7,24 +7,24 @@ asm_stencil_function:
     push rbp
     mov rbp, rsp
 
-    mov rdi, rcx        ; X array pointer
-    mov rsi, rdx        ; Y array pointer
-    mov edx, r8d        ; n
+    mov rdi, rcx        
+    mov rsi, rdx        
+    mov edx, r8d        
 
     xor rax, rax       
 
     mov ecx, 3         
 
 loop_start:
-    mov rax, qword [rdi+rcx*8-24]      ; X[i-3]
-    add rax, qword [rdi+rcx*8-16]      ; X[i-2]
-    add rax, qword [rdi+rcx*8-8]       ; X[i-1]
-    add rax, qword [rdi+rcx*8]         ; X[i]
-    add rax, qword [rdi+rcx*8+8]       ; X[i+1]
-    add rax, qword [rdi+rcx*8+16]      ; X[i+2]
-    add rax, qword [rdi+rcx*8+24]      ; X[i+3]
+    mov rax, qword [rdi+rcx*8-24]      ; i-3
+    add rax, qword [rdi+rcx*8-16]      ; i-2
+    add rax, qword [rdi+rcx*8-8]       ; i-1
+    add rax, qword [rdi+rcx*8]         ; i
+    add rax, qword [rdi+rcx*8+8]       ; i+1
+    add rax, qword [rdi+rcx*8+16]      ; i+2
+    add rax, qword [rdi+rcx*8+24]      ; i+3
 
-    mov qword [rsi+rcx*8-24], rax      ; Store in Y[i]
+    mov qword [rsi+rcx*8-24], rax     
 
     inc ecx                           
     cmp ecx, edx                      
